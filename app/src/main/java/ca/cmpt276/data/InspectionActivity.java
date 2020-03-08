58f352cf751da298170acae5c5ce92f1c053c25d
ramneetb <ramneet_brar@sfu.ca>
2020-03-08 13:39:05 -0700

    InspectionsActivity completed.

--- app/src/main/java/ca/cmpt276/data/InspectionActivity.java
+++ app/src/main/java/ca/cmpt276/data/InspectionActivity.java
@@ -2,9 +2,17 @@ package ca.cmpt276.data;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class InspectionActivity extends AppCompatActivity {
+
+    public static Intent makeLaunchIntent(Context context) {
+        Intent intent = new Intent(context, InspectionActivity.class);
+
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
