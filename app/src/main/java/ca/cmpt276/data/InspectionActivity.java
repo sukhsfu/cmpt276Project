938783b7e97d7b2300692bb8ea595fa386fad6e1
ramneetb <ramneet_brar@sfu.ca>
2020-03-08 20:02:50 -0700

    set up launch intent for inspectionActivity.

--- app/src/main/java/ca/cmpt276/data/InspectionActivity.java
+++ app/src/main/java/ca/cmpt276/data/InspectionActivity.java
@@ -6,17 +6,39 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class InspectionActivity extends AppCompatActivity {
+import java.util.List;
 
-    public static Intent makeLaunchIntent(Context context) {
+import ca.cmpt276.model.Inspection;
+import ca.cmpt276.model.Restaurant;
+import ca.cmpt276.model.RestaurantManager;
+
+public class InspectionActivity extends AppCompatActivity {
+    RestaurantManager manager = RestaurantManager.getInstance();
+    Restaurant restaurant;
+    Inspection inspection;
+
+    public static Intent makeLaunchIntent(Context context, int resPosition, int insPosition) {
         Intent intent = new Intent(context, InspectionActivity.class);
+        intent.putExtra("InspectionActivity_resPos", resPosition);
+        intent.putExtra("InspectionActivity_insPos", insPosition);
 
         return intent;
+    }
+
+    public void extractDataFromIntent() {
+        Intent intent = getIntent();
+        int resPosition = intent.getIntExtra("InspectionActivity_resPos", 0);
+        int insPosition = intent.getIntExtra("InspectionActivity_insPos", 0);
+
+        restaurant = manager.retrieve(resPosition);
+        inspection = restaurant.getInspections().get(insPosition);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inspection);
+
+        extractDataFromIntent();
     }
 }
