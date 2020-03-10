54ac7fcfa864198d9a741fad8f37bb5f7a4112bd
Manpreet Gill <mkb10@sfu.ca>
2020-03-09 22:17:02 -0700

    Updated brief descriptions to fit one line

--- app/src/main/java/ca/cmpt276/model/ViolationManager.java
+++ app/src/main/java/ca/cmpt276/model/ViolationManager.java
@@ -52,28 +52,28 @@ public class ViolationManager implements Iterable<ViolationsBriefDescription> {
         addDescription(new ViolationsBriefDescription(209, "Food might get contaminated"));
         addDescription(new ViolationsBriefDescription(210, "Food not thawed correctly"));
         addDescription(new ViolationsBriefDescription(211, "Frozen food stored above -18 \u00B0 c"));
-        addDescription(new ViolationsBriefDescription(212, "Didn't provide acceptable written procedures"));
-        addDescription(new ViolationsBriefDescription(301, "Equipment not maintained in sanitary condition"));
-        addDescription(new ViolationsBriefDescription(302, "Equipment not properly washed / sanitized"));
-        addDescription(new ViolationsBriefDescription(303, "Hot & cold water not adequate for sanitation"));
+        addDescription(new ViolationsBriefDescription(212, "Unacceptable written procedures"));
+        addDescription(new ViolationsBriefDescription(301, "Unsanitized equipment"));
+        addDescription(new ViolationsBriefDescription(302, "Equipment not properly washed"));
+        addDescription(new ViolationsBriefDescription(303, "Hot & cold water not adequate"));
         addDescription(new ViolationsBriefDescription(304, "Premises not free of pests"));
-        addDescription(new ViolationsBriefDescription(305, "Conditions allowing entrance of pests"));
+        addDescription(new ViolationsBriefDescription(305, "Possible entrance of pests"));
         addDescription(new ViolationsBriefDescription(306, "Premises not sanitized"));
-        addDescription(new ViolationsBriefDescription(307, "Not suitable design/material of equipment"));
-        addDescription(new ViolationsBriefDescription(308, "Equipment not in good working order"));
-        addDescription(new ViolationsBriefDescription(309, "Chemical cleansers stored/labeled improperly "));
-        addDescription(new ViolationsBriefDescription(310, "Single use containers used more than once"));
-        addDescription(new ViolationsBriefDescription(311, "Premises not maintained as per approved plans"));
-        addDescription(new ViolationsBriefDescription(312, "Unnecessary items stored on premises"));
+        addDescription(new ViolationsBriefDescription(307, "Unsuitable equipment material"));
+        addDescription(new ViolationsBriefDescription(308, "Equipment not in good order"));
+        addDescription(new ViolationsBriefDescription(309, "Cleansers stored improperly "));
+        addDescription(new ViolationsBriefDescription(310, "Over-use of single use containers"));
+        addDescription(new ViolationsBriefDescription(311, "Premises not maintained"));
+        addDescription(new ViolationsBriefDescription(312, "Unnecessary items stored"));
         addDescription(new ViolationsBriefDescription(313, "Live animal on premises"));
-        addDescription(new ViolationsBriefDescription(314, "Not provided written sanitation procedures"));
-        addDescription(new ViolationsBriefDescription(315, "Cold/hot holding  equipment lack thermometers"));
+        addDescription(new ViolationsBriefDescription(314, "No written sanitation procedures"));
+        addDescription(new ViolationsBriefDescription(315, "Equipment lack thermometers"));
         addDescription(new ViolationsBriefDescription(401, "Handwashing stations not available"));
-        addDescription(new ViolationsBriefDescription(402, "Employee does not wash hands properly"));
-        addDescription(new ViolationsBriefDescription(403, "Employee lack good personal hygiene"));
-        addDescription(new ViolationsBriefDescription(404, "Employee smoking in prohibited areas"));
-        addDescription(new ViolationsBriefDescription(501, "Doesn't have FOODSAFE Level 1 or equipment"));
-        addDescription(new ViolationsBriefDescription(502, "Doesn't have FOODSAFE Level 1 in operator's absence"));
+        addDescription(new ViolationsBriefDescription(402, "Employee doesn't wash hands"));
+        addDescription(new ViolationsBriefDescription(403, "Employee lack personal hygiene"));
+        addDescription(new ViolationsBriefDescription(404, "Smoking in prohibited areas"));
+        addDescription(new ViolationsBriefDescription(501, "Doesn't have FOODSAFE Level 1"));
+        addDescription(new ViolationsBriefDescription(502, "No FOODSAFE in operator's absence"));
     }
 
 }
